13f0389c1915f4fd8a2e19ca1c3e228cdcf0e3fa
Erin Cry <erin.cry@email.com>
2020-07-28 16:53:44 -0700

    added toString and split function, if / else if/ else statements and join method. it's working in the console but still not displaying correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,32 @@
 $(document).ready(function() {
   $("form#answer").submit(function(event){
-    const number = parseInt($("#input").val());
+    event.preventDefault();
+    var number = parseInt($("#input").val());
     var lowEnd = 0;
     var highEnd = number;
     var arr = [];
     while(lowEnd <= highEnd){
     arr.push(lowEnd++);
+    }
+console.log(arr);
+  var arrMod = arr.forEach(function(number,i) {
     
-    arr.forEach(function(number,i) {
-      if (number === 1) arr[i] = "Beep!"; 
-    });
-  } 
+    var digits = number.toString().split("");
+    if (digits.includes ("3")) arr[i] = "Won't you be my neighbor?"; 
+     else if (digits.includes("2")) arr[i] = "Boop!"; 
+     else if (digits.includes ("1")) arr[i] = "Beep!"; 
+     else {
+      return number;
+    
+    };
+   console.log(arrMod); 
+  });
+  return arrMod.join(", ");
+  });
 
- $("#arr").text(arr);
- $("arr").show();
+
+ $("#arrMod").show();
 
  
-  event.preventDefault();
+  
 })
-  })
-
-
-
